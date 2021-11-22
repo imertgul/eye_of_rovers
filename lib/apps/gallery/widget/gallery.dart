a3e479df485a310c80c952165446715679632089
Mert Gül <ibrahimmertgul@gmail.com>
2021-11-23 00:17:18 +0300

    multi picker added

--- lib/apps/gallery/widget/gallery.dart
+++ lib/apps/gallery/widget/gallery.dart
@@ -16,10 +16,16 @@ class Gallery extends StatefulWidget {
 
 class _GalleryState extends State<Gallery> {
   late FilterBloc _filterBloc;
+  late Camera selectedCamera;
+  late int selectedSol;
 
   @override
   void initState() {
-    _filterBloc = FilterBloc(rover: widget.rover)..add(AddFilter('FHAZ', 1));
+    selectedCamera = widget.rover.availableCameras.first;
+    selectedSol = 100;
+
+    _filterBloc = FilterBloc(rover: widget.rover)
+      ..add(AddFilter(selectedCamera, selectedSol));
     super.initState();
   }
 
@@ -28,11 +34,99 @@ class _GalleryState extends State<Gallery> {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Text(widget.rover.name),
+        trailing: CupertinoButton(
+          child: const Text("Add Filter"),
+          padding: const EdgeInsets.all(0),
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                var pickerSol = CupertinoPicker(
+                  scrollController: FixedExtentScrollController(
+                    initialItem: selectedSol,
+                  ),
+                  itemExtent: 32.0,
+                  backgroundColor: Colors.white,
+                  onSelectedItemChanged: (int index) {},
+                  children: List<Widget>.generate(
+                    2000,
+                    (int index) {
+                      return Center(
+                        child: Text('$index'),
+                      );
+                    },
+                  ),
+                );
+
+                var pickerCamera = CupertinoPicker(
+                  scrollController: FixedExtentScrollController(
+                    initialItem:
+                        widget.rover.availableCameras.indexOf(selectedCamera),
+                  ),
+                  itemExtent: 32.0,
+                  backgroundColor: Colors.white,
+                  onSelectedItemChanged: (int index) {},
+                  children: widget.rover.availableCameras
+                      .map((e) => Text(e.toString().split('.').last))
+                      .toList(),
+                );
+
+                return Container(
+                  height: 300.0,
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Expanded(
+                              child: pickerSol,
+                            ),
+                            Expanded(
+                              child: pickerCamera,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          CupertinoButton(
+                              child: const Text('Cancel'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              }),
+                          CupertinoButton(
+                              child: const Text('Add'),
+                              onPressed: () {
+                                selectedCamera = widget.rover.availableCameras[
+                                    pickerCamera
+                                        .scrollController!.selectedItem];
+                                selectedSol =
+                                    pickerSol.scrollController!.selectedItem;
+                                _filterBloc.add(
+                                    AddFilter(selectedCamera, selectedSol));
+                                Navigator.pop(context);
+                              }),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
       child: BlocBuilder(
         bloc: _filterBloc,
         builder: (context, state) {
           if (state is FilterResult) {
+            if (state.photos.isEmpty) {
+              return const Center(
+                  child: Text('There are no photos for given filters'));
+            }
             return Center(
               child: GridView.count(
                 crossAxisCount: 2,
@@ -48,10 +142,9 @@ class _GalleryState extends State<Gallery> {
           } else if (state is FilterError) {
             return Center(child: Text(state.message));
           } else {
-            //for loading state. There is no other states 
-            return const Center(child: CircularProgressIndicator());
+            //for loading state. There is no other states
+            return const Center(child: CupertinoActivityIndicator());
           }
-
         },
       ),
       // child: FutureBuilder<PhotosResponse>(
